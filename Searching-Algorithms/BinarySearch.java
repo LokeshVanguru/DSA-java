98b84a0f17fa53aad3598c575c849f69283379e0
LokeshVanguru <penchalalokesh2004@gmail.com>
2026-01-15 20:29:36 +0530

    Binary Search

--- Searching-Algorithms/BinarySearch.java
+++ Searching-Algorithms/BinarySearch.java
@@ -1,2 +1,61 @@
-Probelm:
-    searching an element using Binary Search
+// Problem:
+//      check if the element is present in the array or not.
+//      using Binary search
+//      if present return true
+//      else return false
+// Note:Binary Search works on Sorted Arrays.
+
+boolean binarySearch(int arr[],int target)
+{
+    int n=arr.length;
+    if(n==0)
+    {
+        return false;
+    }
+    int left=0;
+    int right=n-1;
+    while(left<=right)
+    {
+        int mid=left+(right-left)/2;
+        if(arr[mid]==target)
+        {
+            return true;
+        }
+        else if(arr[mid]>target)
+        {
+            right=mid-1;
+        }
+        else
+            {
+            left=mid+1;
+        }   
+    }
+    return false;
+}
+
+// Time Complexity:O(log(n))
+//every out input decreses by half.
+
+//------Recursive Binary Search
+
+ boolean recursiveBinarySearch(int arr[],int left,int right,int target)
+ {
+    if(left>right){
+        return false;
+    }
+    int mid=left+(right-left)/2;
+    if(arr[mid]==target)
+    {
+        return true;
+    }
+    else if(arr[mid]>target)
+    {
+        return recursiveBinarySearch(arr,left,mid-1,target);
+    }
+     else{
+         return recursiveBinarySearch(arr,mid+1,right,target);
+     }   
+}
+
+//Time Complexity:O(log(n))
+//Space Complexity:O(log(n)) for recursive call stack
