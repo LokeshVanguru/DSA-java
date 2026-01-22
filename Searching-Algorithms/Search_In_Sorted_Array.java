64133b83d320c6c679c558dc6358cd9e8160171c
LokeshVanguru <penchalalokesh2004@gmail.com>
2026-01-22 22:44:28 +0530

    Searching in sorted array

---
+++ Searching-Algorithms/Search_In_Sorted_Array.java
@@ -0,0 +1,52 @@
+// leet--code--->33
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly left rotated 
+// at an unknown index k (1 <= k < nums.length) such that the resulting array is 
+// [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+// For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target,
+//  return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+public int search(int[] arr, int target) {
+        int len=arr.length;
+        int low=0;
+        int high=len-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target)
+            {
+                return mid;
+            }
+            else if(arr[low]<=arr[mid])
+            {
+                if(arr[low]<=target && arr[mid]>target)
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                if(target>arr[mid] && arr[high]>=target)
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;  
+    }
+
+// Time Complexity:O(logn)
+// Space Complexity:O(1)
